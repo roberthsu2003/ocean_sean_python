66f8224fd100116b1fb4f44af2af269ec06ce12e
徐國堂 <roberthsu2003@gmail.com>
2026-04-22 19:30:24 +0800

    modify

--- lesson12/main.py
+++ lesson12/main.py
@@ -1,6 +1,7 @@
-def main():
+def main(): #定義一個main function
     print("Hello from lesson12!")
+    print("這是一個main function")
 
 
 if __name__ == "__main__":
-    main()
+    main() # 呼叫main function
