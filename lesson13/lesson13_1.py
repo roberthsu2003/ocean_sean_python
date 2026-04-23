7e224daa2747f016a333137fd4afb1294880d170
徐國堂 <roberthsu2003@gmail.com>
2026-04-23 19:23:47 +0800

    modify

---
+++ lesson13/lesson13_1.py
@@ -0,0 +1,8 @@
+def mutiply(a1:int, b1:int)->int:
+    return a1 * b1
+
+if __name__ == "__main__":
+    n1 = 5
+    n2 = 10
+    total = mutiply(n1, n2)
+    print(f"{n1}和{n2}相乘的值:{total}")
--- lesson13/main.py
+++ lesson13/main.py
@@ -1,2 +1,6 @@
+def main():
+    print("這是main function")
+
 if __name__ == "__main__":
     print("我被執行了")
+    main()
